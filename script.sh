ae85804aab263f1c29a0524a3a60dffd56206ae9
Ritsuta <112791063+Ritsuta@users.noreply.github.com>
2022-11-28 20:53:38 +0800

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,3 @@
+echo "Hello, $(whoami)! This script is used for Windows 10 to install several programs that I usually install every reset."
+read -p "Press any key to continue..."
+git clone https://github.com/Ritsuta/bashscriptforwindows10
